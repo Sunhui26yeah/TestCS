c08a6018aa4b7b0318021e1c8122da9d7b75c520
HUI SUN <146675818+Sunhui26yeah@users.noreply.github.com>
2024-09-05 21:22:12 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,8 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int x = 4, y =2;
-        y = x*2+y;
-        System.out.println(y);
+
+        System.out.println(powExample.snippet(4,16));
     }
 }git
